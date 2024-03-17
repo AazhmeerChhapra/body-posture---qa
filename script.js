8ae6701df605738e84593e1e562dac0d4bfd56d5
Arsalanbashir831 <arsalanbashir831@gmail.com>
2024-03-18 00:35:41 +0500

    speech fixes

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-click_to_record.addEventListener('click',function(){
-    var speech = true;
-    window.SpeechRecognition = window.webkitSpeechRecognition;
+const convertText = document.getElementById("convert_text");
+const startButton = document.getElementById("click_to_record");
+const stopButton = document.getElementById("click_to_stop");
+let recognition;
+let lastTranscript = '';
 
-    const recognition = new SpeechRecognition();
+startButton.addEventListener('click', function() {
+    recognition = new window.webkitSpeechRecognition();
     recognition.interimResults = true;
+    recognition.continuous = true; // Enable continuous recognition
 
     recognition.addEventListener('result', e => {
         const transcript = Array.from(e.results)
             .map(result => result[0])
             .map(result => result.transcript)
-            .join('')
+            .join('');
 
-        document.getElementById("convert_text").value = transcript;
-        console.log(transcript);
+        if (transcript !== lastTranscript) {
+            convertText.value = transcript; // Update textarea with new transcript
+            lastTranscript = transcript; // Update lastTranscript
+            console.log(transcript);
+        }
     });
-    
-    if (speech == true) {
-        recognition.start();
+
+    recognition.start();
+});
+
+
+stopButton.addEventListener('click', function() {
+    if (recognition) {
+        recognition.stop();
     }
-})
+});
